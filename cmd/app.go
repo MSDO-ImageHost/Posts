bda9028d795c8dabbe0b1be3040a28fa0c6f0bde
Christian <christian@skjerning.eu>
2020-11-16 02:34:31 +0100

    looking into asim/nitro

--- cmd/app.go
+++ cmd/app.go
@@ -1,118 +1,41 @@
 package main
 
 import (
-	"log"
+	"context"
 
-	"github.com/streadway/amqp"
+	"github.com/asim/nitro-plugins/broker/rabbitmq"
+	"github.com/asim/nitro/v3/app/rpc"
 )
 
-func main() {
-
-	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	createQueue, err := ch.QueueDeclare(
-		"posts.create", // name
-		false,          // durable
-		false,          // delete when unused
-		false,          // exclusive
-		false,          // no-wait
-		nil,            // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	getQueue, err := ch.QueueDeclare(
-		"posts.get", // name
-		false,       // durable
-		false,       // delete when unused
-		false,       // exclusive
-		false,       // no-wait
-		nil,         // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	updateQueue, err := ch.QueueDeclare(
-		"posts.update", // name
-		false,          // durable
-		false,          // delete when unused
-		false,          // exclusive
-		false,          // no-wait
-		nil,            // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	deleteQueue, err := ch.QueueDeclare(
-		"posts.delete", // name
-		false,          // durable
-		false,          // delete when unused
-		false,          // exclusive
-		false,          // no-wait
-		nil,            // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	createMsgs, err := ch.Consume(
-		createQueue.Name, // queue
-		"",               // consumer
-		true,             // auto-ack
-		false,            // exclusive
-		false,            // no-local
-		false,            // no-wait
-		nil,              // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	updateMsgs, err := ch.Consume(
-		updateQueue.Name, // queue
-		"",               // consumer
-		true,             // auto-ack
-		false,            // exclusive
-		false,            // no-local
-		false,            // no-wait
-		nil,              // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	getMsgs, err := ch.Consume(
-		getQueue.Name, // queue
-		"",            // consumer
-		true,          // auto-ack
-		false,         // exclusive
-		false,         // no-local
-		false,         // no-wait
-		nil,           // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	deleteMsgs, err := ch.Consume(
-		deleteQueue.Name, // queue
-		"",               // consumer
-		true,             // auto-ack
-		false,            // exclusive
-		false,            // no-local
-		false,            // no-wait
-		nil,              // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	_ = createMsgs
-	_ = updateMsgs
-	_ = getMsgs
-	_ = deleteMsgs
-
-	forever := make(chan bool)
-
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
+// Define a request type
+type Request struct {
+	Name string
 }
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
+// Define a response type
+type Response struct {
+	Message string
+}
+
+// Create your public App Handler
+type Handler struct{}
+
+// Create a public Handler method which takes request, response and returns an error
+func (h *Handler) Call(ctx context.Context, req *Request, rsp *Response) error {
+	rsp.Message = "Hello " + req.Name
+	return nil
+}
+
+func main() {
+	// Create a new App
+	app := rpc.NewApp()
+
+	// Set the App name
+	app.Name("helloworld")
+
+	// Register the Handler
+	app.Handle(new(Handler))
+
+	// Run the App (blocking call)
+	app.Run()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ImageHost/Posts
 go 1.15
 
 require (
-	github.com/streadway/amqp v1.0.0
+	github.com/asim/nitro/v3 v3.4.1
 	go.mongodb.org/mongo-driver v1.4.3
 )
